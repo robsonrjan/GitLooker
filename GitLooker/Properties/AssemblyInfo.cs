b3695cf2af0a6d122e015ab0cea1a87524c4be53
Robertson <47433406+robsonrjan@users.noreply.github.com>
2020-08-29 19:33:13 +0200

    last msi version - next will go .net core :-)

--- GitLooker/Properties/AssemblyInfo.cs
+++ GitLooker/Properties/AssemblyInfo.cs
@@ -37,5 +37,5 @@ using System.Runtime.InteropServices;
 
 public static class AppVersion
 {
-    public const string AssemblyVersion = "4.0.5";
+    public const string AssemblyVersion = "4.0.6";
 }
