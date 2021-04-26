0f2fa345dac757949fab29cf26874d75dc24e910
Robertson <47433406+robsonrjan@users.noreply.github.com>
2021-04-26 19:04:16 +0200

    Fix for StandardError output

--- GitLooker.Services/CommandProcessor/ProcessShell.cs
+++ GitLooker.Services/CommandProcessor/ProcessShell.cs
@@ -19,15 +19,22 @@ namespace GitLooker.Services.CommandProcessor
                     pr.StartInfo = new ProcessStartInfo(command.Exec, command.Args);
                     pr.StartInfo.UseShellExecute = false;
                     pr.StartInfo.RedirectStandardOutput = true;
+                    pr.StartInfo.RedirectStandardError = true;
                     pr.StartInfo.CreateNoWindow = true;
                     pr.Start();
 
                     var reader = pr.StandardOutput;
+                    var errorReader = pr.StandardError;
                     string output;
+
                     while ((output = reader.ReadLine()) != default)
                         result.Add(output);
 
+                    while ((output = errorReader.ReadLine()) != default)
+                        result.Add(output);
+
                     pr.WaitForExit();
+
                 }
 
                 return result;
--- GitLooker/Properties/AssemblyInfo.cs
+++ GitLooker/Properties/AssemblyInfo.cs
@@ -11,5 +11,5 @@
 
 public static class AppVersion
 {
-    public const string AssemblyVersion = "5.1.0";
+    public const string AssemblyVersion = "5.1.1";
 }
